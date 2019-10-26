e88fbc0e075d1d9fb0bf632ae68e5c53909564f2
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-10-26 19:31:50 +0100

    test: add tests for the lintregistry
    Add missing tests for LintRegistry, there isn't much to test here so just ensured the values are
    returned as expected

--- lintrules/src/test/java/com/chesire/lintrules/LintRegistryTests.kt
+++ lintrules/src/test/java/com/chesire/lintrules/LintRegistryTests.kt
@@ -1,3 +1,20 @@
 package com.chesire.lintrules
 
-class LintRegistryTests
+import com.android.tools.lint.detector.api.CURRENT_API
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class LintRegistryTests {
+    @Test
+    fun `issues returns expected list`() {
+        val registry = LintRegistry()
+        assertTrue(registry.issues.contains(DependencyDetector.duplicateDependency))
+        assertTrue(registry.issues.contains(DependencyDetector.lexicographicOrder))
+    }
+
+    @Test
+    fun `api should return CURRENT_API`() {
+        assertEquals(CURRENT_API, LintRegistry().api)
+    }
+}
