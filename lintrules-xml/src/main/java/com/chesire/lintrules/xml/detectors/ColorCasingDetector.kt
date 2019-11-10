10cfc6c72b787247a3d557a8dcd7e99df12cb43e
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-11-10 22:56:06 +0000

    feat: add detector for colorcasing
    Detector picks up any colors defined in XML that are not upper cased, FF00AA is valid, while ff00aa
    is not. The detector has to run over elements values (for colors.xml), and run over attributes (for
    layout files)

---
+++ lintrules-xml/src/main/java/com/chesire/lintrules/xml/detectors/ColorCasingDetector.kt
@@ -0,0 +1,38 @@
+package com.chesire.lintrules.xml.detectors
+
+import com.android.SdkConstants.ATTR_COLOR
+import com.android.tools.lint.detector.api.ResourceXmlDetector
+import com.android.tools.lint.detector.api.XmlContext
+import com.android.utils.forEach
+import com.chesire.lintrules.xml.issues.ColorCasing
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+
+/**
+ * Detector to find issue types of [ColorCasing].
+ */
+class ColorCasingDetector : ResourceXmlDetector() {
+    private val colorRegex = Regex("#[a-fA-F\\d]{3,8}")
+
+    override fun getApplicableElements(): Collection<String> = ALL
+
+    override fun visitElement(context: XmlContext, element: Element) {
+        if (element.nodeName == ATTR_COLOR) {
+            if (shouldReportOn(element.textContent)) {
+                ColorCasing.report(context, element)
+            }
+        } else {
+            element.attributes.forEach { node ->
+                if (shouldReportOn(node.nodeValue)) {
+                    ColorCasing.report(context, node)
+                }
+            }
+        }
+    }
+
+    private fun shouldReportOn(value: String) =
+        value.matches(colorRegex) && value.any { it.isLowerCase() }
+
+    private fun ColorCasing.report(context: XmlContext, node: Node) =
+        context.report(issue, context.getLocation(node), message)
+}
