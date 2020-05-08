05d7a73a57751d0a86a393147114d2415a692e30
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2020-05-08 16:00:17 +0100

    feat: add new interface for the issues to derive from
    Since issues in the common space will need to share their details with other issues in the other
    modules, add a new interface that allows any other issues to implement to store its basic info. In
    this case the MultipleSpaceIssue will implement the LintIssue to set the basic details such as
    priority and id, and then the MultipleSpaceIssue will be implemented by another class to set the
    Issue field

---
+++ lintrules-common/src/main/java/com/chesire/lintrules/common/issues/LintIssue.kt
@@ -0,0 +1,47 @@
+package com.chesire.lintrules.common.issues
+
+import com.android.tools.lint.detector.api.Category
+import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.Severity
+
+interface LintIssue {
+    /**
+     * The id of the issue, see [Issue.create].
+     */
+    val id: String
+
+    /**
+     * The description of the issue, see [Issue.create].
+     */
+    val briefDescription: String
+
+    /**
+     * The explanation of the issue, see [Issue.create].
+     */
+    val explanation: String
+
+    /**
+     * The category of the issue, see [Issue.create].
+     */
+    val category: Category
+
+    /**
+     * The priority of the issue, see [Issue.create].
+     */
+    val priority: Int
+
+    /**
+     * The severity of the issue, see [Issue.create].
+     */
+    val severity: Severity
+
+    /**
+     * The issue in question, created using [Issue.create].
+     */
+    val issue: Issue
+
+    /**
+     * Message to display when reporting on this issue.
+     */
+    val message: String
+}
---
+++ lintrules-common/src/main/java/com/chesire/lintrules/common/issues/MultipleSpaceIssue.kt
@@ -0,0 +1,25 @@
+package com.chesire.lintrules.common.issues
+
+import com.android.tools.lint.detector.api.Category
+import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.Severity
+
+/**
+ * [Issue] for when there are multiple spaces in a file.
+ */
+interface MultipleSpaceIssue : LintIssue {
+    override val id: String
+        get() = "MultipleSpace"
+    override val briefDescription: String
+        get() = "Detected the use of more than one space"
+    override val explanation: String
+        get() = "Stylistic choice to remove duplicated spaces"
+    override val category: Category
+        get() = Category.CORRECTNESS
+    override val priority: Int
+        get() = 1
+    override val severity: Severity
+        get() = Severity.INFORMATIONAL
+    override val message: String
+        get() = "Multiple spaces should not be used."
+}
