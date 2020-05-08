1e66368b4fd543b4cfdf6e005fa56faf919f4cef
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2020-05-08 21:11:06 +0100

    test: add tests for the MultipleSpaceDetector
    Add a test for the failure case and a test for the success case

--- lintrules-gradle/src/test/java/com/chesire/lintrules/gradle/detectors/MultipleSpaceDetectorTests.kt
+++ lintrules-gradle/src/test/java/com/chesire/lintrules/gradle/detectors/MultipleSpaceDetectorTests.kt
@@ -2,20 +2,19 @@ package com.chesire.lintrules.gradle.detectors
 
 import com.android.tools.lint.checks.infrastructure.TestFiles
 import com.android.tools.lint.checks.infrastructure.TestLintTask
-import com.chesire.lintrules.gradle.issues.LexicographicDependencies
 import com.chesire.lintrules.gradle.issues.MultipleSpace
 import org.junit.Test
 
 class MultipleSpaceDetectorTests {
     @Test
-    fun `multiple space test`() {
+    fun `multipleSpace should be no issue with valid Gradle file`() {
         TestLintTask
             .lint()
             .allowMissingSdk()
             .files(
                 TestFiles.gradle(
                     """
-apply  plugin: 'java-library'
+apply plugin: 'java-library'
 apply plugin: 'kotlin'
 
 dependencies {
@@ -23,7 +22,7 @@ dependencies {
 
     compileOnly "org.jetbrains.kotlin:kotlin-stdlib-jdk7:0.0.0"
 
-    compileOnly  "com.android.tools.lint:lint-api:0.0.0"
+    compileOnly "com.android.tools.lint:lint-api:0.0.0"
     compileOnly "com.android.tools.lint:lint-checks:0.0.0"
 
     testImplementation "com.android.tools.lint:lint:0.0.0"
@@ -41,5 +40,56 @@ jar {
             .issues(MultipleSpace.issue)
             .run()
             .expectClean()
+    }
+
+    @Test
+    fun `multipleSpace should flag issues`() {
+        TestLintTask
+            .lint()
+            .allowMissingSdk()
+            .files(
+                TestFiles.gradle(
+                    """
+apply  plugin: 'java-library'
+apply plugin: 'kotlin'
+
+dependencies {
+    implementation project(path:  ":lintrules-common")
+
+    compileOnly "org.jetbrains.kotlin:kotlin-stdlib-jdk7:0.0.0"
+
+    compileOnly  "com.android.tools.lint:lint-api:0.0.0"
+    compileOnly "com.android.tools.lint:lint-checks:0.0.0"
+
+    testImplementation "com.android.tools.lint:lint:0.0.0"
+    testImplementation "com.android.tools.lint:lint-tests:0.0.0"
+}
+
+jar {
+    manifest  {
+        attributes('Lint-Registry-v2': 'com.chesire.lintrules.gradle.GradleLintRegistry')
+    }
+}
+                """.trimIndent()
+                ).indented()
+            )
+            .issues(MultipleSpace.issue)
+            .run()
+            .expect(
+                """
+                |build.gradle:1: Information: Multiple spaces should not be used. [MultipleSpace]
+                |apply  plugin: 'java-library'
+                |     ~~
+                |build.gradle:5: Information: Multiple spaces should not be used. [MultipleSpace]
+                |    implementation project(path:  ":lintrules-common")
+                |                                ~~
+                |build.gradle:9: Information: Multiple spaces should not be used. [MultipleSpace]
+                |    compileOnly  "com.android.tools.lint:lint-api:0.0.0"
+                |               ~~
+                |build.gradle:17: Information: Multiple spaces should not be used. [MultipleSpace]
+                |    manifest  {
+                |            ~~
+                |0 errors, 0 warnings""".trimMargin()
+            )
     }
 }
