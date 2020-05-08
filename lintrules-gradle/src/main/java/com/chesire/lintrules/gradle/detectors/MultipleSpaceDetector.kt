4ec3deb03ad29296c593970ed922189fef19500f
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2020-05-08 16:11:11 +0100

    feat: add initial files for the MultipleSpace issue in the gradle module
    Add the initial issue object, and the detector file

---
+++ lintrules-gradle/src/main/java/com/chesire/lintrules/gradle/detectors/MultipleSpaceDetector.kt
@@ -0,0 +1,7 @@
+package com.chesire.lintrules.gradle.detectors
+
+import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.GradleScanner
+
+class MultipleSpaceDetector : Detector(), GradleScanner {
+}
