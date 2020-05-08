fae402cd72d0edc89cc3b905603b1d9d8559acd5
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2020-05-08 22:09:08 +0100

    test: add tests to the LintRegistrys
    Add test to ensure the correct issues are present

--- lintrules-gradle/src/test/java/com/chesire/lintrules/gradle/GradleLintRegistryTests.kt
+++ lintrules-gradle/src/test/java/com/chesire/lintrules/gradle/GradleLintRegistryTests.kt
@@ -3,6 +3,7 @@ package com.chesire.lintrules.gradle
 import com.android.tools.lint.detector.api.CURRENT_API
 import com.chesire.lintrules.gradle.issues.DuplicateDependency
 import com.chesire.lintrules.gradle.issues.LexicographicDependencies
+import com.chesire.lintrules.gradle.issues.MultipleSpace
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
@@ -13,6 +14,7 @@ class GradleLintRegistryTests {
         val registry = GradleLintRegistry()
         assertTrue(registry.issues.contains(DuplicateDependency.issue))
         assertTrue(registry.issues.contains(LexicographicDependencies.issue))
+        assertTrue(registry.issues.contains(MultipleSpace.issue))
     }
 
     @Test
