88c6268d2e41c3d1a18cf7c2b1874c0c91c45d42
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2020-05-08 18:03:35 +0100

    refactor: change to use regex for finding double spaces
    Using a regex is much less code and allows easily finding the exact location of where the issues
    occur

--- lintrules-gradle/src/main/java/com/chesire/lintrules/gradle/detectors/MultipleSpaceDetector.kt
+++ lintrules-gradle/src/main/java/com/chesire/lintrules/gradle/detectors/MultipleSpaceDetector.kt
@@ -7,28 +7,26 @@ import com.android.tools.lint.detector.api.Location
 import com.chesire.lintrules.gradle.issues.MultipleSpace
 
 class MultipleSpaceDetector : Detector(), GradleScanner {
+    private val spaceRegex = Regex("\\w[ ]{2,}")
+
     override val customVisitor = true
 
     override fun visitBuildScript(context: Context) {
-        val contents = context.getContents()
-        val lines = contents?.lines()
-        var lineNumber = 0
-        lines?.forEach { line ->
-            val trimmed = line.trim()
-            if (trimmed.contains("  ")) {
-                context.report(
-                    MultipleSpace.issue,
-                    Location.create(
-                        context.file,
-                        contents.toString(),
-                        lineNumber
-                    ),
-                    MultipleSpace.message,
-                    null
-                )
-            }
+        val contents = context.getContents() ?: return
 
-            lineNumber++
+        spaceRegex.findAll(contents).forEach { result ->
+            // This should contain autofix data to replace "  " with just " "
+            context.report(
+                MultipleSpace.issue,
+                Location.create(
+                    context.file,
+                    contents,
+                    result.range.first + 1,
+                    result.range.last + 1
+                ),
+                MultipleSpace.message,
+                null
+            )
         }
     }
 }
