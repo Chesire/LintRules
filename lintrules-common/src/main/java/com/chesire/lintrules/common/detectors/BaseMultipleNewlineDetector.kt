0399099f1ec64e27025203834144769899d3a504
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2020-05-11 19:56:00 +0100

    docs: add missing kdoc for the BaseMultipleNewlineDetector
    Add the kdoc to the class

--- lintrules-common/src/main/java/com/chesire/lintrules/common/detectors/BaseMultipleNewlineDetector.kt
+++ lintrules-common/src/main/java/com/chesire/lintrules/common/detectors/BaseMultipleNewlineDetector.kt
@@ -5,8 +5,15 @@ import com.android.tools.lint.detector.api.Detector
 import com.android.tools.lint.detector.api.GradleScanner
 import com.android.tools.lint.detector.api.XmlContext
 import com.android.tools.lint.detector.api.XmlScanner
+import com.chesire.lintrules.common.issues.MultipleNewlineIssue
 import org.w3c.dom.Document
 
+/**
+ * Provides a base class for other [MultipleNewlineIssue] detectors to use.
+ * This base class will do all of the logic required to detect the usage of multiple newlines across
+ * different file types, consumers need to provide an implementation of the [Context.report] method
+ * which this will call internally.
+ */
 abstract class BaseMultipleNewlineDetector : Detector(), GradleScanner, XmlScanner {
     override fun visitBuildScript(context: Context) {
         val contents = context.getContents() ?: return
