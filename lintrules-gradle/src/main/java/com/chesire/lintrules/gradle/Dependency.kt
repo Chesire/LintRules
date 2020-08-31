db911b5846e62e63931dd4ffc19d54c0ba8eb1c8
Troy Rijkaard <Chesire@users.noreply.github.com>
2020-08-31 20:42:48 +0100

    fix: duplicate dependency on a nested project would fail check (#89)
    Projects that were added to the project via `implementation project(path: ":features:discover")`
    would fail to be correctly checked as a duplicate dependency and would always be flagged as true, 
    due to the parsing only checking the first part of the path and not the whole thing. Update the
    Dependency model to have a method to check which is a bit smarter. As part of this cleaned up some
    of the logic in a few areas.
    
    Closes #88

--- lintrules-gradle/src/main/java/com/chesire/lintrules/gradle/Dependency.kt
+++ lintrules-gradle/src/main/java/com/chesire/lintrules/gradle/Dependency.kt
@@ -11,11 +11,32 @@ data class Dependency(
      */
     val type: String,
     /**
+     * The domain of the dependency.
+     *
+     * Ex: project or "".
+     */
+    val domain: String,
+    /**
      * Name of the dependency.
      */
     val name: String,
     /**
      * Version of the dependency.
      */
-    val version: String
-)
+    val version: String,
+    /**
+     * Full value provided of the [name] & [version].
+     */
+    val value: String
+) {
+    /**
+     * Checks if [other] is the same dependency as this one.
+     */
+    fun sameAs(other: Dependency): Boolean {
+        return if (domain.isNotBlank()) {
+            domain == other.domain && type == other.type && value == other.value
+        } else {
+            type == other.type && name == other.name
+        }
+    }
+}
